e2820807eb72bdc061d62d5ac31069db899a65da
Filipe Sabino <32543907+CRFilipesabino@users.noreply.github.com>
2019-04-23 13:59:37 -0300

    Chat Bugado

---
+++ JavaApplication/src/Cliente/src/com/teste/app/Cliente.java
@@ -0,0 +1,29 @@
+
+package Cliente.src.com.teste.app;
+
+import Cliente.src.com.teste.app.JAnela.ClienteFrame;
+
+
+public class Cliente {
+
+    
+    public static void main(String args[]) {
+        
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ClienteFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+       
+
+        
+        java.awt.EventQueue.invokeLater(() -> {
+            new ClienteFrame().setVisible(true);
+        });
+    }
+}
---
+++ JavaApplication/src/Servidor/src/com/teste/app/Servidor.java
@@ -0,0 +1,13 @@
+
+package Servidor.src.com.teste.app;
+
+import Servidor.src.com.teste.app.service.ServidorService;
+
+
+public class Servidor {
+
+   
+    public static void main(String[] args) {
+        new ServidorService();
+    }
+}
